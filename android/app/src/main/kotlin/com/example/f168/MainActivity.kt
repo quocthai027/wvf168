576bf2e59fa295db8e433ac4cb46d29a7f08c025
Thái Lê <quocthaiadsax0@gmail.com>
2025-02-18 08:58:41 +0700

    push code

---
+++ android/app/src/main/kotlin/com/example/f168/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.f168
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..setJavaScriptMode(JavaScriptMode.unrestricted)
+      ..setNavigationDelegate(
+        NavigationDelegate(
+          onNavigationRequest: (NavigationRequest request) {
+            // Luôn mở trang web trong WebView thay vì trình duyệt ngoài
+            return NavigationDecision.navigate;
+          },
+        ),
+      )
+      ..loadRequest(Uri.parse("https://f16861.com/?id=714427889&currency=VND&type=2"));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        body: SafeArea(
+          child: WebViewWidget(controller: _controller),
+        ),
+      ),
+    );
+  }
+}
